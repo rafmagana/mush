35702d813b5a57f1048e6a3121c4ad1998f34996
Noel Dellofano <noel@zendesk.com>
2010-12-13 16:33:37 -0800

    Added support for Ow.ly url shortening

---
+++ lib/mush/services/owly.rb
@@ -0,0 +1,44 @@
+module Mush
+  
+  module Services
+    
+  require 'json'
+    ## ow.ly
+    # apiKey=I6jj368UlrsrkCKxeLXUU
+    # longUrl=http%3A%2F%2Fbetaworks.com%2F
+    # format=json
+    # domain=j.mp
+    class Owly < AuthenticatedService
+      
+      base_uri 'http://ow.ly/api/1.1/url'
+      format :json
+
+      #Ow.ly api bug, response is in json, but response type is html so force parse
+      def self.response_parse
+        Proc.new do |body, format|
+          JSON.parse(body)
+        end
+      end
+      parser response_parse
+        
+      def shorten(url)
+        invalid_uri_msg = "Please provide a valid URI, including http://"
+        invalid_auth_msg = "Invalid Authorization Data, please provide apikey"
+        
+        raise InvalidURI.new invalid_uri_msg if url.empty?
+        raise InvalidAuthorizationData.new(invalid_auth_msg) if apikey.empty?
+        
+        options = {:format => 'json'}
+        options[:query] = {:apiKey => self.apikey, :longUrl => url}
+        
+        response = get('/shorten', options)
+      
+        puts response.code
+        response.code == 200 ? response["results"]["shortUrl"] : response["error"]
+      end
+      
+    end
+    
+  end
+  
+end
